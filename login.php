e563c36244be2d74a7e97fe844d7ed6088c1094e
Ezra Dowd <ezra.dowd@gmail.com>
2015-11-16 17:34:27 -0500

    working on login

--- login.php
+++ login.php
@@ -8,7 +8,21 @@
 	
 	function doLogin(){
 		$dbcon = getDBConn();
-		
+		stmt = $dbcon->prepare("SELECT `pword` from `users` WHERE `email` = :email");
+		$stmt->bindParam(':email', $_POST["email"]);
+		$stmt->execute();
+		if($stmt->rowCount() > 0) {
+			//There should only ever be one row retrieved since no user should be able to
+			//register with the same email
+			$row = $stmt->fetch();
+			if(password_verify($_POST["password"],$row["pword"])){
+				//WE did it fam
+			} else {
+				//Invalid password
+			}
+		} else {
+			//That email was not found since no rows were returned
+		}
 	}
 	
 	function doRegister(){
