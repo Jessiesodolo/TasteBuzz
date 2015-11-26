03e2f4eb71f6c6ffd64b95ce9b7cdd6657123257
Jessie Sodolo <sodolj@rpi.edu>
2015-11-26 16:46:22 -0500

    Content changes

--- Home.php
+++ Home.php
@@ -4,7 +4,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Launch Me Up Free Theme</title>
+    <title>TasteBUZZ the Taste made for you</title>
     <meta name="description" content="This one page example has a fixed navbar and full page height sections. Each section is vertically centered on larger screens, and then stack responsively on smaller screens. Scrollspy is used to activate the current menu item. This layout also has a contact form example. Uses animate.css, FontAwesome, Google Fonts (Lato and Bitter) and Bootstrap." />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="generator" content="Codeply">
@@ -44,7 +44,7 @@
         </div> 
         <div class="navbar-collapse collapse" id="navbar-collapsible">
             <ul class="nav navbar-nav navbar-left">
-                <li><a href="#section2">Purpose</a></li>
+                <li><a href="#section2">How It Works</a></li>
                 <li><a href="#section3">Learn More</a></li>
                 <li>&nbsp;</li>
             </ul>
@@ -56,7 +56,7 @@
 <section class="container-fluid" id="section1">
     <div class="v-center">
         <!-- <h1 class="text-center">TasteBUZZ</h1> -->
-        <img src="resources/images/Logo_.PNG" class="img-responsive" alt="logo1" style="position:relative; left:33.3%;">
+        <img src="resources/images/Logo_.PNG" class="img-responsive center-block" alt="logo1">
         <h2 class="text-center lato animate slideInDown">Don't have an account?<a href="#" id"Join" style="color:white;"><b style="text-decoration: underline;"> Join Now</b></a></h2>
         <p class="text-center">
             <br>
@@ -80,10 +80,10 @@
                     <div class="col-sm-12 text-center">
                         <div class="panel panel-default slideInLeft animate">
                             <div class="panel-heading">
-                                <h3>Robust</h3></div>
+                                <h3>Simple</h3></div>
                             <div class="panel-body">
-                                <p>There is other content and snippets of details or features that can be placed here..</p>
-                                <hr>GO
+                                <p>We'll ask about your drink preferences and then suggest drinks you might like!</p>
+                                <hr>FIND
                                 <hr>
                             </div>
                         </div>
@@ -95,10 +95,10 @@
                     <div class="col-sm-12 text-center">
                         <div class="panel panel-default slideInUp animate">
                             <div class="panel-heading">
-                                <h3>Simple</h3></div>
+                                <h3>Customized</h3></div>
                             <div class="panel-body">
-                                <p>There is other content and snippets of details or features that can be placed here..</p>
-                                <hr>GO
+                                <p>You can change your preferences to find what your drinks oh choice are</p>
+                                <hr>YOUR
                                 <hr>
                             </div>
                         </div>
@@ -110,10 +110,10 @@
                     <div class="col-sm-12 text-center">
                         <div class="panel panel-default slideInRight animate">
                             <div class="panel-heading">
-                                <h3>Clean</h3></div>
+                                <h3>Instant</h3></div>
                             <div class="panel-body">
-                                <p>There is other content and snippets of details or features that can be placed here..</p>
-                                <hr>GO
+                                <p>Preferences are searched via database and matches are displayed to you!</p>
+                                <hr>BUZZ
                                 <hr>
                             </div>
                         </div>
@@ -135,7 +135,7 @@
             <div class="col-sm-2 col-sm-offset-2 col-xs-6">
                 <div class="text-center">
                     <a href="">
-                        <img style="width:100px;" class="img-circle img-responsive center-block" src="//placehold.it/100/222/?text=FLAT">
+                        <img style="width:100px;" class="img-circle img-responsive center-block" src="//placehold.it/100/222/?text=ALE">
                     </a>
                     <h3 class="text-center"></h3>
                 </div>
@@ -143,7 +143,7 @@
             <div class="col-sm-2 col-xs-6">
                 <div class="text-center">
                     <a href="">
-                        <img style="width:100px;" class="img-circle img-responsive center-block" src="//placehold.it/100/222/?text=FLAT">
+                        <img style="width:100px;" class="img-circle img-responsive center-block" src="//placehold.it/100/222/?text=Vodka">
                     </a>
                     <h3 class="text-center"></h3>
                 </div>
@@ -151,7 +151,7 @@
             <div class="col-sm-2 col-xs-6">
                 <div class="text-center">
                     <a href="">
-                        <img style="width:100px;" class="img-circle img-responsive center-block" src="//placehold.it/100/222/?text=FLAT">
+                        <img style="width:100px;" class="img-circle img-responsive center-block" src="//placehold.it/100/222/?text=Rum">
                     </a>
                     <h3 class="text-center"></h3>
                 </div>
@@ -159,7 +159,7 @@
             <div class="col-sm-2 col-xs-6">
                 <div class="text-center">
                     <a href="">
-                        <img style="width:100px;" class="img-circle img-responsive center-block" src="//placehold.it/100/222/?text=FLAT">
+                        <img style="width:100px;" class="img-circle img-responsive center-block" src="//placehold.it/100/222/?text=Wine">
                     </a>
                     <h3 class="text-center"></h3>
                 </div>
@@ -169,7 +169,7 @@
             <div class="col-sm-2 col-sm-offset-2 col-xs-6">
                 <div class="text-center">
                     <a href="">
-                        <img style="width:100px;" class="img-circle img-responsive center-block" src="//placehold.it/100/222/?text=FLAT">
+                        <img style="width:100px;" class="img-circle img-responsive center-block" src="//placehold.it/100/222/?text=Cider">
                     </a>
                     <h3 class="text-center"></h3>
                 </div>
@@ -177,7 +177,7 @@
             <div class="col-sm-2 col-xs-6">
                 <div class="text-center">
                     <a href="">
-                        <img style="width:100px;" class="img-circle img-responsive center-block" src="//placehold.it/100/222/?text=FLAT">
+                        <img style="width:100px;" class="img-circle img-responsive center-block" src="//placehold.it/100/222/?text=Tequila">
                     </a>
                     <h3 class="text-center"></h3>
                 </div>
@@ -185,7 +185,7 @@
             <div class="col-sm-2 col-xs-6">
                 <div class="text-center">
                     <a href="">
-                        <img style="width:100px;" class="img-circle img-responsive center-block" src="//placehold.it/100/222/?text=FLAT">
+                        <img style="width:100px;" class="img-circle img-responsive center-block" src="//placehold.it/100/222/?text=WHISKEY">
                     </a>
                     <h3 class="text-center"></h3>
                 </div>
@@ -193,7 +193,7 @@
             <div class="col-sm-2 col-xs-6">
                 <div class="text-center">
                     <a href="">
-                        <img style="width:100px;" class="img-circle img-responsive center-block" src="//placehold.it/100/222/?text=FLAT">
+                        <img style="width:100px;" class="img-circle img-responsive center-block" src="//placehold.it/100/222/?text=Gin">
                     </a>
                     <h3 class="text-center"></h3>
                 </div>
@@ -204,29 +204,29 @@
 </section>
 
 <section class="container-fluid" id="section3">
-    <h1 class="text-center">A Bootstrap Foundation</h1>
+    <h1 class="text-center">Discover your BUZZ</h1>
     <div class="row">
         <div class="col-sm-6 col-sm-offset-3">
-            <h3 class="text-center lato slideInUp animate">That <strong>Doesn't</strong> Have to Look Like Bootstrap.</h3>
+            <h3 class="text-center lato slideInUp animate">Finding the <strong>TASTE</strong> that suits you.</h3>
             <br>
             <div class="row">
-                <div class="col-xs-4 col-xs-offset-1">Some brand-tacular designs even have home page content that is taller that 12,000 pixels. That's a lotta content.</div>
+                <div class="col-xs-5 col-xs-offset-1"><p>TASTEBUZZ is intended to help newcomers to the drinking world discover which drinks fit their needs.</p></div>
                 <div class="col-xs-2"></div>
-                <div class="col-xs-4 text-right">Anyhoo, this is just some random blurb of text, and Bootply.com is a playground and code editor for Bootstrap.</div>
+                <div class="col-xs-5 "><p>The data behind our suggestions is from experience, personal testimonies and product research.</p></div>
             </div>
             <br>
             <p class="text-center">
-                <img src="//placehold.it/444x222/444/FFF" class="img-responsive thumbnail center-block ">
+                <img src="resources/images/fire-bomb.jpg" class="img-responsive thumbnail center-block ">
             </p>
         </div>
     </div>
 </section>
 
-    <!--scripts loaded here-->
+    <!--scripts loaded here onfocus="if(this.value == 'value') { this.value = ''; }"    <script src="resources/js/scripts.js"></script> -- >
     
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     
-    <script src="js/scripts.js"></script>
+   
   </body>
 </html>
