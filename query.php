97c3ac63844dc1dde65e2ec23971fb2d3a6fafee
Ezra Dowd <ezra.dowd@gmail.com>
2015-11-30 16:28:19 -0500

    Added functionality for new dinfo table, added functionality for new columns in adding drinks in admin_query.php

--- query.php
+++ query.php
@@ -7,9 +7,9 @@
 	
 	function getRandomDrink(){
 		$dbconn = getDBConn();
-		$id_range = $dbconn->query("SELECT COUNT(*) FROM `dnames`")->rowCount();
+		$id_range = $dbconn->query("SELECT COUNT(*) FROM `dinfo`")->rowCount();
 		$to_select = rand(0,$id_range);
-		$drink = $dbconn-query("SELECT `dname` FROM `dnames` WHERE `id` = $to_select")->fetch();
+		$drink = $dbconn-query("SELECT `dname` FROM `dinfo` WHERE `id` = $to_select")->fetch();
 		echo "{\"drink_name\" : ".$drink["dname"]."}";
 	}
 	
@@ -20,7 +20,7 @@
 		$bestSimilarity = -1;
 		$bestDrink = null;
 		$bestDrinkTraits = array();
-		foreach($dbconn->query("SELECT * FROM `dnames`") as $drinkNameRow){
+		foreach($dbconn->query("SELECT * FROM `dinfo`") as $drinkNameRow){
 			$drinkID = $drinkNameRow["id"];
 			$drinkTraits = $dbconn->query("SELECT * FROM `traits` WHERE id = ".$drinkID)->fetchAll();
 			//Array of format [[id,trait][id,trait]]
@@ -44,9 +44,17 @@
 		echo "{\"drink_name\" : ".$bestDrink."\"drink_traits\" : ".json_encode($bestDrinkTraits)."}";
 	}
 	
+	function getDrinkInfo(){
+		$drinkName = $_POST["drinkName"];
+		$stmt = $dbconn->prepare("SELECT * FROM `dinfo` WHERE `dname` = :dname");
+		$stmt->bindParam(':dname', $drinkName);
+		$stmt->execute();
+		echo json_encode($stmt->fetch());
+	}
+	
 	function getDrinkTraits(){
 		$drinkName = $_POST["drinkName"];
-		$stmt = $dbconn->prepare("SELECT `id` FROM `dname` WHERE `dname` = :dname");
+		$stmt = $dbconn->prepare("SELECT `id` FROM `dinfo` WHERE `dname` = :dname");
 		$stmt->bindParam(':dname', $drinkName);
 		$stmt->execute();
 		$did = $stmt->fetch()["id"];
@@ -59,7 +67,7 @@
 	
 	function getAllDrinks(){
 		$dArray = array();
-		foreach($dbconn->query("SELECT * FROM `dnames`") as $drinkNameRow){
+		foreach($dbconn->query("SELECT * FROM `dinfo`") as $drinkNameRow){
 			array_push($dArray,$drinkNameRow["dname"]);
 		}
 		echo json_encode($dArray);
@@ -79,7 +87,7 @@
 		$userID = $_SESSION['uid'];
 		$userPreferences = $dbconn->query("SELECT * FROM `userprefs` WHERE id = ".$userID)->fetchAll();
 		$simArray = array();
-		foreach($dbconn->query("SELECT * FROM `dnames`") as $drinkNameRow){
+		foreach($dbconn->query("SELECT * FROM `dinfo`") as $drinkNameRow){
 			$drinkID = $drinkNameRow["id"];
 			$drinkTraits = $dbconn->query("SELECT * FROM `traits` WHERE id = ".$drinkID)->fetchAll();
 			//Array of format [[id,trait][id,trait]]
