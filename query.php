a822dd526418b9fb906691482f4b4fdd2115a378
BuildTools <unconfigured@null.spigotmc.org>
2015-11-30 20:51:24 -0500

    Fixing query.php

--- query.php
+++ query.php
@@ -52,7 +52,7 @@
 		$result->closeCursor();
 		foreach($drinks as $drinkNameRow){
 			$drinkID = $drinkNameRow["id"];
-			$drinkTraits = $dbconn->query("SELECT * FROM `traits` WHERE `id` = ".$drinkID)->fetchAll();
+			$drinkTraits = $dbconn->query("SELECT * FROM `dtraits` WHERE `id` = ".$drinkID)->fetchAll();
 			//Array of format [[id,trait][id,trait]]
 			$currentSimilarity = 0;
 			foreach($userPreferences as $userP){
